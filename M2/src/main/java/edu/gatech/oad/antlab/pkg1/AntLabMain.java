0448af84f66f35cd4d1f72444737b958a7db843d
Ryan Chiang <ryanfunny8@gmail.com>
2015-09-01 15:57:32 -0400

    Added my name to AntMain

--- M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
+++ M2/src/main/java/edu/gatech/oad/antlab/pkg1/AntLabMain.java
@@ -61,7 +61,7 @@ import edu.gatech.oad.antlab.pkg2.*;
 		  toPrint += p1.toString("hlane6");
 		  //Person2 replace P2 with your name
 		  //and gburdell with your gt id
-		  Person2 p2 = new Person2("P2");
+		  Person2 p2 = new Person2("Ryan Chiang");
 		  toPrint += p2.toString("rchiang1");
 		  //Person3 replace P3 with your name
 		  //and gburdell3 with your gt id
