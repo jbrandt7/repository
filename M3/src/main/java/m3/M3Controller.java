6c7b7021317f89fbfd4a59f0e705d9a11a18535b
Harry <hlane6@gatech.edu>
2015-09-14 23:37:12 -0400

    Updated game config screen, store number of players

--- M3/src/main/java/m3/M3Controller.java
+++ M3/src/main/java/m3/M3Controller.java
@@ -10,14 +10,19 @@ import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.*;
 import javafx.collections.FXCollections;
 
 public class M3Controller implements Initializable {
 
     @FXML
     private Button toConfigButton;
+
+    @FXML
+    private Slider playersSlider;
+
+    @FXML
+    private ChoiceBox mapChoiceBox, difficultyChoiceBox;
 
     @FXML
     public void toConfigScreen(ActionEvent event) throws Exception {
@@ -43,6 +48,7 @@ public class M3Controller implements Initializable {
     @FXML
     public void toPlayerSetUp(ActionEvent event) throws Exception {
         Parent parent = FXMLLoader.load(getClass().getResource("/m3/view/PlayerSelection.fxml"));
+        M3.setPlayerCount((int) playersSlider.getValue());
         M3.changeScene(new Scene(parent), "Player Configuration");
     }
 
@@ -57,22 +63,26 @@ public class M3Controller implements Initializable {
 
     @Override
     public void initialize(URL arg0, ResourceBundle arg1) {
-        assert p1RaceChoiceBox != null : "choicebox not injected";
+        if (mapChoiceBox != null) {
+            mapChoiceBox.setItems(FXCollections.observableArrayList(
+                        "Mars", "Venus", "Mars"));
+        }
+        if (difficultyChoiceBox != null) {
+            difficultyChoiceBox.setItems(FXCollections.observableArrayList(
+                        "Easy", "Medium", "Hard"));
+        }
         if (p1RaceChoiceBox!= null) {
             p1RaceChoiceBox.setItems(FXCollections.observableArrayList(
                     "Human", "Protoss", "Zerg"));
         }
-        assert p2RaceChoiceBox != null : "choicebox not injected";
         if (p2RaceChoiceBox!= null) {
             p2RaceChoiceBox.setItems(FXCollections.observableArrayList(
                     "Human", "Protoss", "Zerg"));
         }
-        assert p3RaceChoiceBox != null : "choicebox not injected";
         if (p3RaceChoiceBox!= null) {
             p3RaceChoiceBox.setItems(FXCollections.observableArrayList(
                     "Human", "Protoss", "Zerg"));
         }
-        assert p4RaceChoiceBox != null : "choicebox not injected";
         if (p4RaceChoiceBox!= null) {
             p4RaceChoiceBox.setItems(FXCollections.observableArrayList(
                     "Human", "Protoss", "Zerg"));
