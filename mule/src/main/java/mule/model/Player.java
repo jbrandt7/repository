9dbdda7c17ceb1ac00ef70891f580f18cb3f1c1d
jbrandt7 <jbrandt7@gatech.edu>
2015-09-20 22:40:14 -0400

    Added Player.java to the repository

--- mule/src/main/java/mule/model/Player.java
+++ mule/src/main/java/mule/model/Player.java
@@ -1,7 +1,38 @@
-package mule.model;
+/***************************/
+/**Player Class*************/
+/**Last updated: 9/15/2015**/
 
-/**
- * Created by harrylane on 9/16/15.
- */
 public class Player {
+	private String name;
+	private String race;
+	private int money;
+	private ResourceBag bag;
+	private Mule mule;
+	private Plot[] land;
+	private int[][] location;
+	
+	public Player(String name, String race, int money, ResourceBag bag) {
+		this.name = name;
+		this.race = race;
+		this.money = money;
+		this.bag = bag;
+		this.mule = null;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	public String getRace() {
+		return race;
+	}
+	public int getMoney() {
+		return money;
+	}
+	public int addMoney(int m) {
+		money += m;
+		return money;
+	}
+	public int removeMoney(int m) {
+	}
+
 }
