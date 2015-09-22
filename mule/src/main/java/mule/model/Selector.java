98b6769cfb79f4165990765b5be37cd54eea96a7
jbrandt7 <jbrandt7@gatech.edu>
2015-09-22 16:49:28 -0400

    added move to Selector and Enum Direction

--- mule/src/main/java/mule/model/Selector.java
+++ mule/src/main/java/mule/model/Selector.java
@@ -19,4 +19,31 @@ public class Selector {
     public void setPoint(Point p) {
         this.p = p;
     }
+
+    public void move(Direction d) {
+        switch (d) {
+            case UP:
+                if (p.getY() > 0) {
+                    p.setLocation(p.getX(), p.getY() - 75);
+                }
+                break;
+            case RIGHT:
+                if (p.getX() < 525) {
+                    p.setLocation(p.getX() + 75, p.getY());
+                }
+                break;
+            case DOWN:
+                if (p.getY() < 525) {
+                    p.setLocation(p.getX(), p.getY() + 75);
+                }
+                break;
+            case LEFT:
+                if (p.getX() > 0) {
+                    p.setLocation(p.getX() - 75, p.getY());
+                }
+                break;
+            default:
+                break;
+        }
+    }
 }
